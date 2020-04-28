8cf8c75dd055e5976e9e7904d4a8065211389d72
陈振旭 <chen_zhenxu@cdv.com>
2020-04-28 11:52:15 +0800

    修改注释

--- SKOpenGLUtils/Classes/Utils/SKShaderHelper.h
+++ SKOpenGLUtils/Classes/Utils/SKShaderHelper.h
@@ -10,17 +10,15 @@
 
 @interface SKShaderHelper : NSObject
 
-/**
- 将一个顶点着色器和一个片段着色器挂载到一个着色器程序上，并返回程序的 id
- 
- @param shaderName 着色器名称，顶点着色器应该命名为 shaderName.vsh ，片段着色器应该命名为 shaderName.fsh
- @return 着色器程序的 ID
- */
+/// 通过一张图片来创建纹理
+/// @param image 图片
++ (GLuint)createTextureWithImage:(UIImage *)image;
+
+/// 将一个顶点着色器和一个片段着色器挂载到一个着色器程序上，并返回程序的 id
+/// @param shaderName  着色器名称，顶点着色器应该命名为 shaderName.vsh ，片段着色器应该命名为 shaderName.fsh
+/// @param bundle 资源Bundle
 + (GLuint)programWithShaderName:(NSString *)shaderName
                  resourceBundle:(NSBundle *)bundle;
-
-// 通过一张图片来创建纹理
-+ (GLuint)createTextureWithImage:(UIImage *)image;
 
 /// 编译shader
 /// @param name 着色器名称
