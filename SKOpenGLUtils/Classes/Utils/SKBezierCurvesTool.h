e81b7ef97475d2972fcea0ae43d977944cfa05ed
烧烤 <zhenxu056@126.com>
2021-01-07 11:40:29 +0800

    添加文件

---
+++ SKOpenGLUtils/Classes/Utils/SKBezierCurvesTool.h
@@ -0,0 +1,27 @@
+//
+//  MFBezierCurvesTool.h
+//  GLPaintViewDemo
+//
+//  Created by Lyman Li on 2019/9/25.
+//  Copyright © 2019年 Lyman Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SKBezierCurvesTool : NSObject
+
+/**
+ 通过二次贝塞尔曲线的三个关键点，计算点序列
+
+ @param from 起始点
+ @param to 终止点
+ @param control 控制点
+ @param pointSize 画笔尺寸，用于计算生成点的数量
+ @return 点序列
+ */
++ (NSArray <NSValue *>*)pointsWithFrom:(CGPoint)from
+                                    to:(CGPoint)to
+                               control:(CGPoint)control
+                             pointSize:(CGFloat)pointSize;
+
+@end
